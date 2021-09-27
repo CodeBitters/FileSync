7187ac17de4d1d9adf83a1c2759ee107a1bd01d8
Chathura Wanniarachchi <wachathuraj@gmail.com>
2021-09-27 23:46:15 +0530

    Database class implemented.

---
+++ src/DBConnection.java
@@ -0,0 +1,68 @@
+import org.json.simple.JSONObject;
+
+import java.sql.*;
+import java.util.Stack;
+
+public class DBConnection {
+
+    private Connection connection;
+
+    public DBConnection() {
+        this.connection = null;
+    }
+
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:data/file_db.sqlite");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+    public Stack<JSONObject> executeSelectQuery(String query) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet queryResult = statement.executeQuery(query);
+        Stack<JSONObject> returnData = new Stack<JSONObject>();
+        while (queryResult.next()) {
+            JSONObject dataObject = new JSONObject();
+            dataObject.put("is_select", true);
+            dataObject.put("file_id", queryResult.getInt("file_id"));
+            dataObject.put("file_name", queryResult.getString("file_name"));
+            dataObject.put("source_full_path", queryResult.getString("source_full_path"));
+            dataObject.put("destination_full_path", queryResult.getString("destination_full_path"));
+            dataObject.put("updated_at", queryResult.getDate("updated_at").toString());
+            dataObject.put("hash", queryResult.getString("hash"));
+            dataObject.put("is_active", queryResult.getBoolean("is_active"));
+            returnData.push(dataObject);
+        }
+        return returnData;
+    }
+
+    public Stack<JSONObject> executeOtherQuery(String query) {
+        Statement statement;
+        JSONObject dataObject = new JSONObject();
+        dataObject.put("is_select", false);
+        try {
+            statement = connection.createStatement();
+            statement.execute(query);
+            dataObject.put("query_status", "1");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            dataObject.put("query_status", "0");
+        }
+
+        Stack<JSONObject> returnData = new Stack<JSONObject>();
+        returnData.push(dataObject);
+        return returnData;
+    }
+
+    public Object insertDataEntry(String name, String source, String destination, String fileHash) {
+        String sqlQuery = "insert into file_info (file_name, source_full_path, destination_full_path, updated_at, hash, is_active) values (" +
+                "'" + name + "','" + source + "','" + destination + "',datetime(strftime('%s','now'), 'unixepoch', 'localtime'),'" + fileHash + "',1 );";
+        Stack<JSONObject> result = this.executeOtherQuery(sqlQuery);
+        return result.get(0).get("query_status").equals("1");
+    }
+}
--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,4 +1,8 @@
+import org.json.simple.JSONObject;
+
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Stack;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -8,5 +12,17 @@ public class ServerMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+//        TODO initially create file list in database
+//        TODO identify changes
+//        DBConnection dbConnection=new DBConnection();
+//        dbConnection.connect();
+//        try {
+//            Object r = dbConnection.insertDataEntry("hunny","hello/3","hello/2","23232");
+//            System.out.println(r);
+//            Stack<JSONObject> data = dbConnection.executeSelectQuery("select * from file_info");
+//            System.out.println(data);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 }
